4caf5ef83b47126b49ca2214e4f15cc378a20a01
Diego <Diego@Master-Notebook>
2014-01-25 11:15:05 +0100

    Merge master/master

--- console.php
+++ console.php
@@ -4,7 +4,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <!--css -->
         <link rel="stylesheet" href="style.css" type="text/css" />
-<<<<<<< HEAD
         <!-- jquery e jquery mobile -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/latest/jquery.js"></script>
@@ -20,33 +19,14 @@
         ?>
             <article>
                 <p id="path"> Home > Shop > Console </p>
-                <h1>
-                Console
-                </h1>
-=======
-        <link rel="stylesheet" href="stylemio.css" type="text/css" />
-        <!-- jquery e jquery mobile -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js"></script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/latest/jquery.js"></script>
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css" />
-        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script>
-        <title>MetaGame: Console</title>
-    </head>
-    <body>
-        <div data-role="page" id="first_page">
-        <?php
-        include 'header.php';
-        ?>
-            <article>
-                <p id="path"> Home > Shop > Console </p>
-                <h1>
-                Console
-                </h1>
-                <a href="giochi.php"> <img src="img/console/Scheda_Console_ps4.png" class="catecons"/> </a>
-                <a href="giochi.php"> <img src="img/console/Scheda_Console_Xbox.png" class="catecons"/> </a>
-                <a href="giochi.php"> <img src="img/console/Scheda_Console_Wii.png" class="catecons"/> </a>
-
+                <div id="conteinerCenter">
+                    <h1>
+                    Console
+                    </h1>
+                    <a href="giochi.php"> <img src="img/console/Scheda_Console_ps4.png" class="catecons"/> </a>
+                    <a href="giochi.php"> <img src="img/console/Scheda_Console_Xbox.png" class="catecons"/> </a>
+                    <a href="giochi.php"> <img src="img/console/Scheda_Console_Wii.png" class="catecons"/> </a>
+                </div>
             </article>
         
         <?php
